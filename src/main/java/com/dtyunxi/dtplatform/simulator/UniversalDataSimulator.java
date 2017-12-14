451c39ebfe557849731912ec455dc0ef69b13e9d
zhang.guohao <1353535195@qq.com>
2017-12-14 09:16:20 +0800

    "commit local"

--- src/main/java/com/dtyunxi/dtplatform/simulator/UniversalDataSimulator.java
+++ src/main/java/com/dtyunxi/dtplatform/simulator/UniversalDataSimulator.java
@@ -1,7 +1,31 @@
 package com.dtyunxi.dtplatform.simulator;
 
-public class UniversalDataSimulator {
+import com.dtyunxi.dtplatform.model.Config;
+import com.dtyunxi.dtplatform.timer.EndTimer;
+import com.dtyunxi.dtplatform.timer.StartTimer;
+import com.dtyunxi.dtplatform.utils.*;
+import org.apache.log4j.Logger;
 
+import java.util.Date;
+import java.util.Timer;
 
+/**
+ * 入口类
+ */
+public class UniversalDataSimulator{
 
+    public static Logger logger = Logger.getLogger(UniversalDataSimulator.class);
+    public static void main(String[] args) {
+        if (args.length<1){
+            logger.error("Usage:config file");
+            System.exit(1);
+        }
+        String configFile = args[0];
+        Config config = ConfigUtils.getConfig(configFile);
+        Date startTime = DateUtils.getDate(config.getStartTime());
+        Date endTime = DateUtils.getDate(config.getEndTime());
+        Timer timer = new Timer();
+        timer.schedule(new StartTimer(config),startTime);
+        timer.schedule(new EndTimer(),endTime);
+    }
 }
