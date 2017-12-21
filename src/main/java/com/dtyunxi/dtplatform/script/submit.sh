8bcc69f09d5c36b185f4ad761c111e0e3bb33822
zhang.guohao <1353535195@qq.com>
2017-12-21 11:59:54 +0800

    latest

--- src/main/java/com/dtyunxi/dtplatform/script/submit.sh
+++ src/main/java/com/dtyunxi/dtplatform/script/submit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 date=`date +%Y%m%d%H`
-java -jar /home/admin/zhangguohao/simulator/apps/universal-simulator-dtplatform-1.0-SNAPSHOT-jar-with-dependencies.jar /home/admin/zhangguohao/simulator/config.json >>/home/admin/zhangguohao/simluator/logs/${date}.log
+java -jar {USER_HOME}/universal-simulator-dtplatform-1.0-SNAPSHOT-jar-with-dependencies.jar {USER_HOME}/simulator/config.json >>{USER_HOME}/simluator/logs/${date}.log
